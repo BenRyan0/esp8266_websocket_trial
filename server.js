3a1c5d21edc8d61b59dacdb554cf3f3bd89d94f5
BenRyan0 <benryanrinconada5@gmail.com>
2025-03-16 12:37:30 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws, req) => {
+    console.log('Client connected from:', req.socket.remoteAddress);
+
+    ws.on('message', (message) => {
+        try {
+            const data = JSON.parse(message);
+            console.log('Received message:', data);
+        } catch (error) {
+            console.error('Invalid JSON received:', message);
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('WebSocket server is running');
+});
+
+server.listen(8080, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:8080');
+});
